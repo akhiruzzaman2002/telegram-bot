d52105e1e38b1d32195108a6b87d00c9624a2b8d
akhiruzzaman2002 <mdakhiruzzaman2002@gmail.com>
2025-09-20 01:55:58 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-echo "🚀 Installing dependencies..."
 pip install -r requirements.txt
-
-if [ -z "$1" ]; then
-  echo "⚠️ Usage: bash start.sh <lite_bot.py|full_bot.py>"
-  exit 1
-fi
-
-echo "🤖 Starting $1 ..."
-python $1
+echo "🤖 Starting lite_bot.py ..."
+python lite_bot.py
